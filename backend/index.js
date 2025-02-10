54facd2c188c96d295929a97b4692b414ce4d439
Rohit Handage <106050288+Rohithandage@users.noreply.github.com>
2025-02-10 23:17:40 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const router = require('./routes')
 
 const app = express()
 app.use(cors({
-    origin :"https://sparksage-2.onrender.com",
+    origin : process.env.FRONTEND_URL,
     credentials : true
 }))
 app.use(express.json())
